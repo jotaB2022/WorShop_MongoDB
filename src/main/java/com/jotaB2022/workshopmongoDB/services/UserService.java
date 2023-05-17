25daddd21613af44424d3907904e8ebba324706b
jotaB <joaobatista1999estudo@gmail.com>
2023-05-17 17:02:09 -0300

    Obtendo um usurio pelo id, e seu tratamento de erro

--- src/main/java/com/jotaB2022/workshopmongoDB/services/UserService.java
+++ src/main/java/com/jotaB2022/workshopmongoDB/services/UserService.java
@@ -1,12 +1,14 @@
 package com.jotaB2022.workshopmongoDB.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.jotaB2022.workshopmongoDB.domain.User;
 import com.jotaB2022.workshopmongoDB.repositories.UserRepository;
+import com.jotaB2022.workshopmongoDB.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -18,4 +20,22 @@ public class UserService {
 	public List<User> findAll(){
 	return repo.findAll();
 	}
+	
+	public User findById(String id) {
+	Optional <User> obj = repo.findById(id);
+	return obj.orElseThrow(()-> new ObjectNotFoundException("Objeto não encontrado"));
 }
+	
+}
+
+/*
+public User findById(String id) {
+	Optional <User> user = repo.findById(id);
+	if (user == null) {
+	 throw new ObjectNotFoundException("Objeto não encontrado");
+	}
+	return user.get();
+	}
+	*/
+
+
