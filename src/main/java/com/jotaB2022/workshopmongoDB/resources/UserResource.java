b1f7346e895c7a21afae992bf4f4717d73b8e272
jotaB <joaobatista1999estudo@gmail.com>
2023-05-19 00:56:33 -0300

    Endpoint para Retornar um Usuario

--- src/main/java/com/jotaB2022/workshopmongoDB/resources/UserResource.java
+++ src/main/java/com/jotaB2022/workshopmongoDB/resources/UserResource.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.jotaB2022.workshopmongoDB.domain.Post;
 import com.jotaB2022.workshopmongoDB.domain.User;
 import com.jotaB2022.workshopmongoDB.dto.UserDTO;
 import com.jotaB2022.workshopmongoDB.services.UserService;
@@ -37,7 +38,7 @@ public class UserResource {
 
 	}
 
-	@GetMapping(value = "/{id}")
+	@GetMapping(value ="/{id}")
 	public ResponseEntity<UserDTO> findById(@PathVariable String id) {
 
 		User user = service.findById(id);
@@ -71,5 +72,11 @@ public class UserResource {
 		return ResponseEntity.noContent().build();
 
 	}
+	
+	@GetMapping(value = "/{id}/posts")
+	public ResponseEntity<List<Post>> FindyPostinUser(@PathVariable String id) {
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj.getPosts());
 
+}
 }
